411ad1cf8039b14f4c3c0e4d8022b3368b5107d6
joshNic <256jomu@gmail.com>
2018-08-14 23:11:37 +0300

    add get single question endpoint  [Delivers #159778612]

--- app/api.py
+++ app/api.py
@@ -5,24 +5,34 @@ app = Flask(__name__)
 # Sample data of questions
 questions = [{
     "questionId": 1,
-    "title" : "What is does Error 404 mean",
-    "body" : "So i came across this when i was trying to access my old website"
+    "title": "What is does Error 404 mean",
+    "body": "So i came across this when i was trying to access my old website"
+},
+    {
+    "questionId": 2,
+    "title": "What does Error 401 mean",
+    "body": "I have this application which always returns this error please help"
 }]
 
 
-@app.route('/', methods=['GET'])
+@app.route('/api/v1', methods=['GET'])
 def home():
     return jsonify({'message': 'It works'})
 
+# return all questions endpoint
 
-@app.route('/questions', methods=['GET'])
+
+@app.route('/api/v1/questions', methods=['GET'])
 def fetch_all():
-    pass
+    return jsonify({'questions': questions})
 
 
-@app.route('/question/<int:questionId>', methods=['GET'])
+@app.route('/api/v1/question/<int:questionId>', methods=['GET'])
 def fetch_one(questionId):
-    pass
+    question = [
+        question for question in questions if question['questionId'] == questionId]
+    return jsonify({'question': question[0]})
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
