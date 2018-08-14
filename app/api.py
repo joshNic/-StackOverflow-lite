adb213ec2c89dd235cd8a5f270c34894561e5c10
joshNic <256jomu@gmail.com>
2018-08-14 23:31:04 +0300

    add errorhandler for question not in list  [Delivers #159778612]

--- app/api.py
+++ app/api.py
@@ -1,5 +1,4 @@
-from flask import Flask, jsonify, request, Response
-
+from flask import Flask, jsonify, request, Response, abort, make_response
 app = Flask(__name__)
 
 # Sample data of questions
@@ -20,18 +19,22 @@ def home():
     return jsonify({'message': 'It works'})
 
 # return all questions endpoint
-
-
 @app.route('/api/v1/questions', methods=['GET'])
 def fetch_all():
     return jsonify({'questions': questions})
 
-
+# get singl question endpoint
 @app.route('/api/v1/question/<int:questionId>', methods=['GET'])
 def fetch_one(questionId):
     question = [
         question for question in questions if question['questionId'] == questionId]
+    if len(question) == 0:
+        abort(404)
     return jsonify({'question': question[0]})
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error':'Question not found'}), 404)
 
 
 if __name__ == '__main__':
