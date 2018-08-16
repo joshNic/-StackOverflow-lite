b84f5e441187b1fa8f78f72a6cc6d4e7b2c2f860
joshNic <256jomu@gmail.com>
2018-08-16 15:28:01 +0300

    add post question [Delivers #159820921]

--- app/api.py
+++ app/api.py
@@ -9,8 +9,29 @@ questions = [{
 },
     {
     "questionId": 2,
-    "title": "What does Error 401 mean",
+    "title": "What does Error 400 mean",
     "body": "I have this application which always returns this error please help"
+}]
+
+answers = [{
+    "answerId": 1,
+    "questionId": 1,
+    "answer_body": "It means error resource not found"
+},
+    {
+    "answerId": 2,
+    "questionId": 1,
+    "answer_body": "It means what ever you are trying to find is not around"
+},
+    {
+    "answerId": 3,
+    "questionId": 2,
+    "answer_body": "It means bad request"
+},
+    {
+    "answerId": 4,
+    "questionId": 2,
+    "answer_body": "it means bad request check that your data is properly formated"
 }]
 
 
@@ -51,12 +72,24 @@ def post_question():
         'body': request.json.get('body', "")
     }
     questions.append(question)
-    return jsonify({'question': question}), 201
+    return jsonify({'question': questions}), 201
 
-@app.route('/api/v1/answer', methods=['POST'])
-def post_answer():
-    pass
-
+@app.route('/api/v1/answer/<int:questionId>', methods=['POST'])
+def post_answer(questionId):
+    question = [
+        question for question in questions if question['questionId'] == questionId]
+    if len(question) == 0:
+        abort(404)
+    if not request.json or not 'answer_body' in request.json:
+        abort(400)
+    if question:
+        answer = {
+            'answerId': answers[-1]['answerId'] + 1,
+            'questionId': questionId,
+            'answer_body': request.json.get('answer_body', "")
+        }
+        answers.append(answer)
+    return jsonify({'answer':answers}),201
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
