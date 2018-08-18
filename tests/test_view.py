f4cbe9fec8641f75fa7390799b20b8b7711f397d
joshNic <256jomu@gmail.com>
2018-08-18 19:09:05 +0300

    Modify test_view.py add fetch_one test [ Chore #159869371]

--- tests/test_view.py
+++ tests/test_view.py
@@ -9,3 +9,12 @@ def test_fetch_all(make_response_get_questions):
     assert make_response_get_questions.json
     assert data['questions'][0]['title'] == "What is does Error 404 mean"
     assert len(data['questions']) > 2
+
+
+def test_fetch_one(make_response_get_question, make_not_found):
+    data = json.loads(json.dumps(make_response_get_question.json))
+    assert make_response_get_question.status_code == 200
+    assert make_response_get_question.json
+    assert make_not_found.status_code == 404
+    assert data['question']['title'] == "What does Error 400 mean"
+    assert len(data['question']) == 3
